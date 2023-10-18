52513e2bfdd35599202883bcb8e09ae2b1a7abd6
MaxOakov <106341683+MaxOakov@users.noreply.github.com>
2023-10-18 14:43:43 +0300

    Update calculator.py
    Adding changes to the functions of calculator. Reducing the size of functions and optimizing work

--- Lab_1/calculator.py
+++ Lab_1/calculator.py
@@ -1,7 +1,7 @@
 from sys import exit
 
 
-def contin():
+def keep_on():
     i = 1
     while i == 1:
         cont = input('Do you want to continue? (y-Yes/n-No):')
@@ -16,88 +16,98 @@ def contin():
 def history():
     with open("History", "r") as r:
         print(r.read())
-        contin()
+        keep_on()
 
 
 def save(res):
     i = 0
     while i == 0:
-        save = input("Do you want to save (y-Yes/n-No): ")
-        if save == 'y':
+        saving = input("Do you want to save (y-Yes/n-No): ")
+        if saving == 'y':
             with open("History", "a", encoding='utf-8') as w:
                 w.write(str(res) + "\n")
             print("Saving")
             i = 1
-            contin()
-        elif save == 'n':
+            keep_on()
+        elif saving == 'n':
             print("...")
-            contin()
             i = 1
+            keep_on()
         else:
             print("Incorrect symbol try again")
 
 
-def result(a, b, sign, c):
+def result(first_number, second_number, sign, result_number):
     if sign == 's':
-        res = (str(a) + "\u221A" + str(b) + " = " + str(c))
+        res = (str(first_number) + "\u221A" + str(second_number) + " = " + str(result_number))
     else:
-        res = (str(a) + str(sign) + str(b) + " = " + str(c))
+        res = (str(first_number) + str(sign) + str(second_number) + " = " + str(result_number))
     print(res)
     save(res)
 
 
+def sign_poll():
+    valid_signs = ('+', '-', '*', '/', '**', '%', 's', 'h', 'c', 'e')
 
-def calc():
-    i = 1
-    numbers = 1
-    while i == 1:
-        a = float
-        b = float
-        f = int
-        sign = input("Sign (+, -, *, /, **, %, s - √, h - history, c - clear history): ")
-        if sign in ('h', 'c'):
-            if sign == 'h':
-                history()
-            elif sign == 'c':
-                with open("History", "w") as file:
-                    file.write('\n')
-                    contin()
-        elif sign in ('+', '-', '*', '/', '**', '%', 's'):
+    while True:
+        sign = input("Sign (+, -, *, /, **, %, s - √, h - history, c - clear history, e - end of proces): ")
 
-            while numbers == 1:
-                try:
-                    a = float(input("a = "))
-                    b = float(input("b = "))
-                    f = int(input("Input number of float: "))
-                    numbers = 0
-                except ValueError:
-                    print("Use only numbers!")
-            f = "%." + str(f) + "f"
-
-
-            if sign == '+':
-                c = (f % (a + b))
-                result(a, b, sign, c)
-            elif sign == '-':
-                c = (f % (a - b))
-                result(a, b, sign, c)
-            elif sign == '**':
-                c = (f % (a ** b))
-                result(a, b, sign, c)
-            elif sign == '%':
-                c = (f % (a % b))
-                result(a, b, sign, c)
-            elif sign == '*':
-                c = (f % (a * b))
-                result(a, b, sign, c)
-            elif sign == '/':
-                if b != 0:
-                    c = (f % (a / b))
-                    result(a, b, sign, c)
-                else:
-                    print("Division by zero!")
-            elif sign == 's' or '√':
-                c = a ** (1 / b)
-                result(a, b, sign, c)
+        if sign in valid_signs:
+            break
         else:
-            print("Incorrect sign!")
+            print("Wrong sign. Please, input correct sign.")
+
+    if sign in ('h', 'c', 'e'):
+        if sign == 'e':
+            exit(print("End"))
+        elif sign == 'h':
+            history()
+        elif sign == 'c':
+            with open("History", "w") as file:
+                file.write('\n')
+                keep_on()
+    elif sign in ('+', '-', '*', '/', '**', '%', 's'):
+        numbers_poll(sign)
+
+
+def numbers_poll(sign):
+    i = 1
+    while i == 1:
+        while True:
+            try:
+                a = float(input("a = "))
+                b = float(input("b = "))
+                f = int(input("Input number of float: "))
+                break
+            except ValueError:
+                print("Use only numbers!")
+        if sign == "/" and b == 0:
+            print("Division by zero!")
+        else:
+            i = 0
+
+        f = "%." + str(f) + "f"
+
+    calc(a, b, f, sign)
+
+
+def calc(first_number, second_number, float_number, sign):
+    if sign == '+':
+        c = (float_number % (first_number + second_number))
+    elif sign == '-':
+        c = (float_number % (first_number - second_number))
+    elif sign == '**':
+        c = (float_number % (first_number ** second_number))
+    elif sign == '%':
+        c = (float_number % (first_number % second_number))
+    elif sign == '*':
+        c = (float_number % (first_number * second_number))
+    elif sign == '/':
+        try:
+            c = (float_number % (first_number / second_number))
+        except ZeroDivisionError:
+            print("Division by zero!")
+    elif sign == 's' or '√':
+        c = first_number ** (1 / second_number)
+
+    result(first_number, second_number, sign, c)
